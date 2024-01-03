5981ab18e299cad2eff31cace7760626fd881709
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-03 22:24:14 +0530

    model and serialization added

--- mixin_ex/api/admin.py
+++ mixin_ex/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import StudentModel
+
+
+@admin.register(StudentModel)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('id','name','email')
--- mixin_ex/api/models.py
+++ mixin_ex/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class StudentModel(models.Model):
+    name=models.CharField(max_length=15)
+    email=models.EmailField()
+
+    class Meta:
+        db_table='stud'
+    def __str__(self) -> str:
+        return self.name
