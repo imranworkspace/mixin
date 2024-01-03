0180656bd276fcb21d95f24bc02607a39a6933c2
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-03 22:28:58 +0530

    ListModelMixin added

--- mixin_ex/api/views.py
+++ mixin_ex/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import StudentModel
+from .serialization import StudentSerializers
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 # Create your views here.
+
+class StudentList(ListModelMixin,GenericAPIView):
+    queryset=StudentModel.objects.all()
+    serializer_class=StudentSerializers
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    
